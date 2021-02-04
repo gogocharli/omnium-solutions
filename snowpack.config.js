0a640b2185f36102935172cfaeb3457c7e746554
Charles De Mount <thexrayone@icloud.com>
2021-02-03 20:45:15 -0500

    fix: :poop: remedy dev error by changing path in config

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/en', static: true },
+    public: { url: '/', static: true },
     src: { url: '/dist' },
   },
   plugins: [
