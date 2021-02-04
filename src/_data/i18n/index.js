4b5b4971e9da200f6f40f221943b0f84877dc528
Charles De Mount <thexrayone@icloud.com>
2021-02-03 20:45:56 -0500

    feat: :construction: add necessary markup for homepage

--- src/_data/i18n/index.js
+++ src/_data/i18n/index.js
@@ -57,4 +57,46 @@ module.exports = {
       fr: 'Suivez votre progrès',
     },
   },
+  tools: {
+    en: 'Our Tools',
+    fr: 'Nos Outils',
+  },
+  clients: {
+    en: 'Case Studies',
+    fr: "Cas d'Études",
+  },
+  form: {
+    title: {
+      en: 'Start growing faster',
+      fr: 'Commencez votre expansion',
+    },
+    subtitle: {
+      en: "Sales shouldn't be hard",
+      fr: 'La vente ne devrait pas être ardue',
+    },
+    name: {
+      en: 'Full name',
+      fr: 'Nom complet',
+    },
+    company: {
+      en: 'Company',
+      fr: 'Compagnie',
+    },
+    email: {
+      en: 'E-mail',
+      fr: 'Adresse e-mail',
+    },
+    phone: {
+      en: 'Phone number',
+      fr: 'Numéro de tel',
+    },
+    upload: {
+      en: 'Join Resume',
+      fr: 'Attachez CV',
+    },
+    submit: {
+      en: 'Get in touch',
+      fr: 'Envoyer',
+    },
+  },
 };
