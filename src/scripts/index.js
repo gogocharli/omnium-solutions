acd3b64b5604f32aa88fa985045c9620c549b76d
Charles De Mount <thexrayone@icloud.com>
2021-02-10 11:55:53 -0500

    fix(animations): fix hero cls and arrows on slideshow

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,7 +3,11 @@ import { mount, unMount, hideScrollBar, showScrollBar } from './slider.js';
 const mq = window.matchMedia('(min-width: 40em)');
 
 mq.addListener(handleSizeChange);
-mq.matches && hideScrollBar() && mount();
+
+if (mq.matches) {
+  hideScrollBar();
+  mount();
+}
 
 function handleSizeChange(e) {
   if (e.matches) {
